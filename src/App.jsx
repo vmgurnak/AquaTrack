e87bca85e86cccb7e6bdb8cb164f048b604f3f0c
Valeriy.Gurnak <vmgurnak@gmail.com>
2024-08-01 15:58:31 +0300

    updatet

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import css from './App.module.css';
+
+function App() {
+  return (
+    <div className={css.container}>
+      <div>Project AquaTrack</div>
+    </div>
+  );
+}
+
+export default App;
